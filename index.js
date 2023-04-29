061734e47a358d27726339ae3bea33cfd17b85d2
Anmol Git <Anmol>
2023-04-29 22:15:16 +0530

    deploy-2

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ const { userRoute } = require("./Router/user.Route");
 const { auth } = require("./middleware/auth.midleware");
 let cors = require("cors");
 let app = express();
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 app.use(express.json());
 app.get("/", (req, res) => {
